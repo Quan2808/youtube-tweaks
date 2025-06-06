d4b9c9c8fe0dc2b264b5e0ac6572cdaeb339e99a
Nguyễn Trung Quân <116139754+Quan2808@users.noreply.github.com>
2025-06-06 17:15:20 +0700

    feat: auto update version into manifest and release script support by version

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,14 @@ const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ZipPlugin = require("zip-webpack-plugin");
+const fs = require("fs");
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
+
+  // Write version from package.json
+  const packageJson = require("./package.json");
+  const version = packageJson.version;
 
   return {
     mode: argv.mode || "development",
@@ -50,7 +55,16 @@ module.exports = (env, argv) => {
     plugins: [
       new CopyPlugin({
         patterns: [
-          { from: "./src/manifest.json", to: "./" },
+          {
+            from: "./src/manifest.json",
+            to: "./",
+            transform(content) {
+              // Auto update version in manifest.josn
+              const manifest = JSON.parse(content.toString());
+              manifest.version = version;
+              return JSON.stringify(manifest, null, 2);
+            },
+          },
           { from: "./src/assets", to: "./assets", noErrorOnMissing: true },
           { from: "./src/_locales", to: "./_locales", noErrorOnMissing: true },
         ],
@@ -66,8 +80,7 @@ module.exports = (env, argv) => {
       ...(isProduction
         ? [
             new ZipPlugin({
-              filename:
-                "youtube-tweaks-v" + require("./package.json").version + ".zip",
+              filename: `youtube-tweaks-v${version}.zip`,
             }),
           ]
         : []),
